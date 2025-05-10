667440e3243c92cdb597d7771bd322f7c8850c3c
Cortezkevin <A19105415@idat.edu.pe>
2025-05-09 21:48:49 -0500

    [feat] - Agregando pagina de carrito

--- app/cart/page.tsx
+++ app/cart/page.tsx
@@ -1,9 +1,38 @@
+import Stepper from '@/components/ui/cart/Stepper'
+import { Card } from '@heroui/card'
 import { NextPage } from 'next'
 import React from 'react'
 
 const CartPage: NextPage = () => {
   return (
-    <div>CartPage</div>
+    <div>
+      <div>
+        <div>
+          <h2 className='text-2xl font-semibold'>Carrito de Compras</h2>
+          <p className='text-md text-default-500'>Tienes <b className='text-default-800'>2 productos</b> en tu carrito</p>
+        </div>
+        <Card>
+        </Card>
+      </div>
+      <div></div>
+      <Stepper
+        defaultStep={2}
+        steps={[
+          {
+            title: "Carrito",
+          },
+          {
+            title: "Dirección",
+          },
+          {
+            title: "Pago",
+          },
+          {
+            title: "Confirmación",
+          }
+        ]}
+       />
+    </div>
   )
 }
 
