f6afe0a87dd44bc6c9ce4f809437c27c4f4f8c81
Cortezkevin <81886622+Cortezkevin@users.noreply.github.com>
2025-05-09 21:41:43 -0500

    [refactor] - Arreglando conflictos de versiones

--- components/ui/commons/Product.tsx
+++ components/ui/commons/Product.tsx
@@ -1,6 +1,7 @@
 import { Button } from '@heroui/button';
 import { Card, CardBody, CardFooter } from '@heroui/card';
 import { Image } from '@heroui/image';
+import { motion } from 'framer-motion';
 import React, { FC } from 'react'
 
 interface Props {
@@ -11,27 +12,52 @@ interface Props {
 }
 
 export const Product: FC<Props> = ({ name, price, image }) => {
+  
+  const [isHover, setIsHover] = React.useState(false);
+  
   return (
-    <Card shadow="lg" className='w-[290px]'>
-      <CardBody className="p-0 overflow-visible">
-        <Image
-          alt={name}
-          className="w-full object-cover h-[200px]"
-          radius="lg"
-          shadow="sm"
-          src={image}
-          width="100%"
-        />
-      </CardBody>
-      <CardFooter className="justify-between text-small">
-        <b>{name}</b>
-        <div className='flex items-center gap-4'>
-          <p className="text-lg text-default-500">S/ {price}</p>
-          <Button isIconOnly aria-label="Add To Cart" color="warning" variant="flat" className="rounded-full">
-            <i className="fa-solid fa-cart-shopping"></i>
-          </Button>
-        </div>
-      </CardFooter>
-    </Card>
+    <Card shadow="lg" className='w-[292px]' onMouseOver={e => setIsHover(true)} onMouseLeave={e => setIsHover(false)}>
+        <CardBody className="p-0 overflow-visible">
+          <Image
+            alt={name}
+            className="w-full object-cover h-[200px]"
+            radius="none"
+            shadow="sm"
+            src={image}
+            width="100%"
+          />
+        </CardBody>
+        <CardFooter className='relative p-0'>
+          <div className='w-full flex flex-col gap-3 p-3'>
+            <p className='text-lg font-semibold'>{name}</p>
+            <div className='w-full flex justify-between items-center'>
+              <p className="text-lg text-default-500 text-red-500">S/ {price}</p>
+              <div>5 Estrellas</div>
+            </div>
+          </div>
+          <div className='absolute w-full h-full'>
+            <motion.div
+              style={{
+                background: "#ffffffbb",
+                height: "100%",
+                position: "relative",
+                width: "100%",
+              }}
+              initial={{ y: 100 }}
+              animate={ isHover ? { y: 0 } : {}}
+              transition={{ duration: .8, type: "spring"}}
+            >
+              <div className='w-full h-full flex items-center justify-center gap-2'>
+                <Button variant='solid' endContent={<i className="fa-solid fa-eye"></i>}>
+                  Ver
+                </Button>
+                <Button variant='solid' endContent={<i className="fa-solid fa-cart-plus"></i>}>
+                  Add to Cart
+                </Button>
+              </div>
+            </motion.div>
+          </div>
+        </CardFooter>
+      </Card>
   )
 }
